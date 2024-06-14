18a00f5bfff20ef4e3af1a72d6a6c401c3dce67d
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-14 15:21:01 +0530

    sdklnjfjs

--- src/App.js
+++ src/App.js
@@ -32,6 +32,8 @@ function App() {
     fetchWeather();
   }, [query, units]);
 
+
+  
   const formatBackground = () => {
     if (!weather) return "from-cyan-700 to-blue-700";
     const threshold = units === "metric" ? 20 : 60;
