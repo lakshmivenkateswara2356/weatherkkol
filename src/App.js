8e8b65579ebd8462969d77dc3048cffb79fd7025
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-14 15:19:29 +0530

    sjdhfks

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 
-
 function App() {
   const [query, setQuery] = useState({ q: "berlin" });
   const [units, setUnits] = useState("metric");
